fe414a680934489f74507a17454bee0eb5c9c2e7
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2024-11-25 12:06:22 +0300

    Saving changes

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser="Tamara Kaka";
+const welcomeMessage="Welcome to Flatbook, " + currentUser +"!";
+const excitedWelcomeMessage=welcomeMessage.toUpperCase() + currentUser + "!";
+const shortGreeting="Welcome, " + currentUser[0] +"!";
+console.log(shortGreeting);
